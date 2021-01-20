3625c5d6dc61e5ab7a8eb32f9e68c65a1d5f11d0
ceren esirgenci <ceren_esirgenci@hotmail.com>
2021-01-21 00:17:44 +0300

    b

---
+++ OOP/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class ProductManager
+    {
+        public void ADD(Product product)
+        {
+            Console.WriteLine(product.ProductName+ "eklendi");
+        }
+        public void Update(Product product) {
+            Console.WriteLine(product.ProductName + "güncellendi");
+        }
+
+        /*public int Topla(int sayi1, int sayi2) {
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2); 
+        }*/
+    }
+}
